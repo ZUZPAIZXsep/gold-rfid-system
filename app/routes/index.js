9ab0dc4f6d6b6a79297c02b16342b392c51a72d9
ZUZPAIZXsep <s6303051613092@email.kmutnb.ac.th>
2024-05-07 18:40:39 +0700

    Update addpage and rfid module

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,12 +4,13 @@ const mongoose = require('mongoose');
 const dayjs = require('dayjs');
 const { ObjectId } = require('mongoose').Types;
 const connectDB = require('../Config/db');
+const rfidModule = require('../rfid_module/rfidReader');
 
 
 // เชื่อมต่อกับ MongoDB
 mongoose.connect('mongodb+srv://admin:1234@goldcluster.nf1xhez.mongodb.net/GoldRfid', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 const db = mongoose.connection;
 
@@ -85,6 +86,99 @@ router.get('/gold_list', async (req, res, next) => {
   }
 });
 
+// router.get('/add_golddata', async (req,res) => {
+//   try{
+//       let condition = {};
+//       const addgoldsdata = await Gold.find(condition);
+//       res.render('add_golddata', {
+//         addgoldsdata: addgoldsdata,
+//         dayjs: dayjs, 
+//         });
+//   } catch (error) {
+//       console.error(error);
+//       res.status(500).send('Internal Server Error');
+//   }
+// });
+
+router.get('/add_golddata', async (req, res) => {
+  try {
+    let addgoldsdata = [];
+    let rfidTags = rfidModule.getRfidTags(); // เรียกใช้งาน rfidTags จาก rfidReader.js
+    for (let i = 0; i < rfidTags.length; i++) {
+      addgoldsdata.push({ gold_id: rfidTags[i] });
+    }
+    res.render('add_golddata', {
+      addgoldsdata: addgoldsdata,
+      dayjs: dayjs
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/clear_rfid_tags', (req, res) => {
+  try {
+      rfidModule.resetRfidTags(); // เรียกใช้งานฟังก์ชัน resetRfidTags() เพื่อล้างค่า rfidTags
+      res.json({ message: 'RFID Tags cleared successfully' }); // ส่งข้อความกลับไปยัง client เพื่อแสดงว่าล้างค่าสำเร็จ
+      console.log('RFID Tags cleared successfully');
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/gold_edit', async (req,res) => {
+  try{
+      let condition = {};
+
+      // ถ้ามีการเลือกประเภททองคำ
+      if (req.query.select_goldType && req.query.select_goldType !== 'เลือกประเภททองคำ') {
+          condition.goldtype = req.query.select_goldType;
+      }
+
+      // ถ้ามีการเลือกขนาดทองคำ
+      if (req.query.select_goldSize && req.query.select_goldSize !== 'เลือกขนาดทองคำ') {
+          condition.size = req.query.select_goldSize;
+      }
+
+      const goldsedit = await Gold.find(condition);
+      res.render('gold_edit', {
+        goldsedit: goldsedit,
+        dayjs: dayjs, 
+        select_goldType: 
+        req.query.select_goldType, 
+        select_goldSize: req.query.select_goldSize});
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+// // สร้างเส้นทางเพื่อแก้ไขข้อมูล
+// router.get('/edit/:id', async (req, res) => {
+//   try {
+//       const gold_edit = await Gold.findById(req.params.id);
+//       res.render('edit_modal', { gold_edit: gold_edit });
+//   } catch (error) {
+//       console.error(error);
+//       res.status(500).send('Internal Server Error');
+//   }
+// });
+
+// // สร้างเส้นทางเพื่ออัปเดตข้อมูล
+// router.post('/update/:id', async (req, res) => {
+//   try {
+//       const { goldtype, size, weight } = req.body;
+//       await Gold.findByIdAndUpdate(req.params.id, { goldtype, size, weight });
+//       res.redirect('/add_edit');
+//   } catch (error) {
+//       console.error(error);
+//       res.status(500).send('Internal Server Error');
+//   }
+// });
 
 
 
